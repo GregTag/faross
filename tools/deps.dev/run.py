44b19ef358ac4288357e19d5c17dbad7c9aa32c8
Марат Будажапов <budazhapov.mm@phystech.edu>
2024-04-19 20:42:39 +0300

    [DAS-104] partly fix ouput

--- tools/deps.dev/run.py
+++ tools/deps.dev/run.py
@@ -3,18 +3,25 @@ import requests
 from urllib.parse import quote
 
 
+def getResponse(url: str) -> None:
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        sys.stderr.write(f"Запрос к {url} успешно выполнен!")
+        print(response.json())
+    else:
+        sys.stderr.write(f"Произошла ошибка, {response.status_code}\n")
+        sys.stderr.write(response.text)
+        sys.exit(1)
+
+
 purl = sys.argv[1]
 percent_encoded_purl = quote(purl, safe='')
 
 # Только метод PurlLookup поддерживает запрос через Purl
 base_purl_lookup_url = "https://api.deps.dev/v3alpha/purl/"
-purl_lookup_response = requests.get(base_purl_lookup_url + percent_encoded_purl)
+# TODO: JSON не соответсвует образцу
+# TODO: используется alpha версия API
 
-if purl_lookup_response.status_code == 200:
-    print("Запрос PurlLookup успешно выполнен!")
-    print("Ответ сервера:")
-    print(purl_lookup_response.json())
-else:
-    print("Произошла ошибка при выполнении запроса:")
-    print("Код ошибки:", purl_lookup_response.status_code)
-    print("Текст ошибки:", purl_lookup_response.text)
+url = base_purl_lookup_url + percent_encoded_purl
+getResponse(url)
