6ec5b7610be4d458aa942a221e3513f352dacf6d
Olga Akimova <akimova.om@phystech.edu>
2024-04-28 19:30:19 +0300

    [DAS-89] scan all pages

--- issues/run.sh
+++ issues/run.sh
@@ -20,7 +20,19 @@ REPO=$2
 DAYS_AGO="${3:-30}"
 FROM_DATE=$(date -v -${DAYS_AGO}d -u +"%Y-%m-%dT%H:%M:%SZ")
 
-curl "https://api.github.com/repos/$OWNER/$REPO/issues?since=$FROM_DATE" 2> /dev/null | \
-jq 'map(select(.pull_request | not))' > issues.txt
+TEMP_FILE='temp.txt'
 
-jq length issues.txt
+issues_count=0
+page=1
+while : ; do
+    curl "https://api.github.com/repos/$OWNER/$REPO/issues?since=$FROM_DATE&page=$page" 2> /dev/null > $TEMP_FILE
+    received=$(jq length $TEMP_FILE)
+    real_issues=$(jq 'map(select(.pull_request | not)) | length' $TEMP_FILE)
+    ((issues_count+=real_issues))
+    ((page++))
+    [[ $received > 0 ]] || break
+done
+
+rm $TEMP_FILE
+
+echo $issues_count
